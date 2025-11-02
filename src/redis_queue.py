d27b184f7e8012cf919906cfa1f13a144d16c002
Kelvich Bogdan <kelvichrd@gmail.com>
2025-11-02 18:36:02 +0300

    Solve redis queue task

---
+++ src/redis_queue.py
@@ -0,0 +1,32 @@
+import json
+
+import redis
+
+
+class RedisQueue:
+    def __init__(self, queue_name):
+        self._r = redis.Redis(
+            host="localhost",
+            port=6379
+        )
+        self.queue_name = queue_name
+
+    def publish(self, msg: dict):
+        self._r.rpush(self.queue_name, json.dumps(msg))
+
+    def consume(self) -> dict:
+        response = self._r.blpop(self.queue_name)
+        if response:
+            return json.loads(response[1])
+        return None
+
+
+if __name__ == '__main__':
+    q = RedisQueue(queue_name="fifo_queue")
+    q.publish({'a': 1})
+    q.publish({'b': 2})
+    q.publish({'c': 3})
+
+    assert q.consume() == {'a': 1}
+    assert q.consume() == {'b': 2}
+    assert q.consume() == {'c': 3}
